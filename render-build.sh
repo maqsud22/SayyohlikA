2fcc9a674fa53066dd5302038756d9e85c1eab90
maqsud22 <mbaratov876@gmail.com>
2025-11-01 07:06:22 +0500

    Improve Render build: clean + restore + migrate + publish

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,8 @@
-﻿
+﻿#!/usr/bin/env bash
+set -o errexit
+
+# Bazani yangilash (ixtiyoriy, agar migratsiya bo‘lsa)
+dotnet ef database update || echo "Migration skipped"
+
+# Build loyihani
+dotnet publish -c Release -o out
